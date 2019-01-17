ee580fecf62b0af03999a303236895195e542b20
QuloZ <akimsiec@gmail.com>
2019-01-17 21:19:01 +0300

    MainMenu
    MainMenu

---
+++ Assets/Scripts/MainMenu/ButtonLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonLight : MonoBehaviour {
+
+    private SpriteRenderer buttonColor;
+    private Color colorOn;
+    private Color colorOff;
+    
+
+    public void Start()
+    {
+        buttonColor = GetComponent<SpriteRenderer>();
+       
+    }
+
+     void OnMouseEnter()
+    {
+        buttonColor.color = new Color(buttonColor.color.r, buttonColor.color.g, buttonColor.color.b, 255f);
+            
+    }
+     void OnMouseExit()
+    {
+        buttonColor.color = new Color(buttonColor.color.r, buttonColor.color.g, buttonColor.color.b, 0f);
+    }
+}
---
+++ Assets/Scripts/MainMenu/LightGerb.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightGerb : MonoBehaviour {
+
+    private SpriteRenderer gerbColor;
+
+	// Use this for initialization
+	void Start () {
+        gerbColor = GetComponent<SpriteRenderer>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gerbColor.color = new Color(gerbColor.color.r, gerbColor.color.g, gerbColor.color.b,(0.6f + Mathf.PingPong(Time.time / 4.0f, 0.5f)));
+	}
+}
