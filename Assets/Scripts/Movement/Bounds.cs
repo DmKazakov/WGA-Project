b40e960a3f4654c853b867e1f41f2865f97b1bf0
andtarspb <46713402+andtarspb@users.noreply.github.com>
2019-05-01 02:08:52 +0500

    локации в тайлах

---
+++ Assets/Scripts/Movement/Bounds.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounds : MonoBehaviour
+{
+
+    BoxCollider2D bounds;
+    CameraControl camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bounds = GetComponent<BoxCollider2D>();
+        camera = FindObjectOfType<CameraControl>();
+
+        camera.SetBounds(bounds);
+    }
+}
