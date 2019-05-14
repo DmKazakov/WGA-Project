f6fd7dd02c42517e8d50da37185e1c96c5613bec
QuloZ <akimsiec@gmail.com>
2019-05-14 16:24:14 +0300

    Начало диалогов

--- Assets/Scripts/Story/Story.cs
+++ Assets/Scripts/Story/Story.cs
@@ -6,11 +6,19 @@ public static class Story
 {
     public static double chapter = 1.0;
 
+    public static GameObject roomManager;
+    public static GameObject dvorManager;
+    public static GameObject tManager;
+    public static GameObject schoolDvorManager;
+    public static GameObject garageManager;
+
+
     public static void NextChapter() {
         chapter += 0.1;
     }
 
     public static void Restart() {
+        roomManager.GetComponent<RoomManager>().Restart();
         chapter = 1.0;
     }
 }
