2da958d4299ff13d02136f39d22c8fb0e393785c
QuloZ <akimsiec@gmail.com>
2019-05-22 17:21:05 +0300

    Правка всплывающей подсказки + сообщение в CreatePlayer
    Выскакивает сообщение, если не выбрали умения или не распределили хар-ки

---
+++ Assets/Scripts/CreatePlayer/MessageClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageClick : MonoBehaviour
+{
+    public NewGame script;
+
+    public void Apply() {
+        script.StartGame();
+    }
+    public void Cancel() {
+        gameObject.SetActive(false);
+    }
+}
