560d1fcd25921e95a6fbad176120bfab4d174bab
andtarspb <46713402+andtarspb@users.noreply.github.com>
2019-05-24 02:21:45 +0300

    гопники с анимацией на локациях, туториал, новый create player

--- Assets/Scripts/Location/T/TMessage.cs
+++ Assets/Scripts/Location/T/TMessage.cs
@@ -6,8 +6,9 @@ public class TMessage : MessageText
 {
     public override void Init()
     {
-        txt = new string[2];
-        txt[0] = "Это что еще за пылесос?!";
-        txt[1] = "А ну вали отсюда, пока не разобрали!";
+        txt = new string[1];
+        txt[0] = @"Это что еще за пылесос?!
+А ну вали отсюда, пока не разобрали!";
+        //txt[1] = "А ну вали отсюда, пока не разобрали!";
     }
 }
