48437c9f152890f5fcd986c0ad09fb5c3f351f81
QuloZ <akimsiec@gmail.com>
2019-05-23 20:30:34 +0300

    Настроил баррикады на перекрестке

---
+++ Assets/Scripts/Location/T/BandosBarrColl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BandosBarrColl : MonoBehaviour
+{
+   public GameObject message;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        message.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Location/T/TMessage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TMessage : MessageText
+{
+    public override void Init()
+    {
+        txt = new string[2];
+        txt[0] = "Это что еще за пылесос?!";
+        txt[1] = "А ну вали отсюда, пока не разобрали!";
+    }
+}
